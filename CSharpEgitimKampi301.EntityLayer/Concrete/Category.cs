490096d838da471b11eabc82bdcc7b532fc3fb14
Emre Baris Erdem <erdem.emre.baris@gmail.com>
2024-11-04 13:27:38 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+	public class Category
+	{
+        //internal ==> access modifier => Bulunduğu katmandan erişim sağlanır.
+
+        // Public , Private , Internal , Protected
+
+        //Field - Variable - Property
+
+        //int x; // field
+
+        //void metod()
+        //{
+        //    int y; // Variable
+        //}
+
+        public int CategoryId { get; set; } // Property
+
+        public string CategoryName { get; set; }
+
+        public bool CategoryStatus { get; set; }
+
+
+
+
+    }
+}
