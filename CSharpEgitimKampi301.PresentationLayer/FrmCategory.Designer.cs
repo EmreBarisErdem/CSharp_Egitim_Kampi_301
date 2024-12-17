cdfbd8fc135e20a704de7a4ded4aac740c8b48d7
Emre Baris Erdem <erdem.emre.baris@gmail.com>
2024-12-17 22:52:36 +0300

    19_Business Katmanı ve Logic Kurallar

---
+++ CSharpEgitimKampi301.PresentationLayer/FrmCategory.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace CSharpEgitimKampi301.PresentationLayer
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtCategoryID = new System.Windows.Forms.TextBox();
+			this.btnList = new System.Windows.Forms.Button();
+			this.datagridview1 = new System.Windows.Forms.DataGridView();
+			this.txtCategoryName = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.rdbActive = new System.Windows.Forms.RadioButton();
+			this.rdbPassive = new System.Windows.Forms.RadioButton();
+			this.BtnAdd = new System.Windows.Forms.Button();
+			this.BtnDelete = new System.Windows.Forms.Button();
+			this.BtnUpdate = new System.Windows.Forms.Button();
+			this.btnGetById = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.datagridview1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(62, 60);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(93, 20);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Kategori ID:";
+			// 
+			// txtCategoryID
+			// 
+			this.txtCategoryID.Location = new System.Drawing.Point(164, 54);
+			this.txtCategoryID.Name = "txtCategoryID";
+			this.txtCategoryID.Size = new System.Drawing.Size(198, 26);
+			this.txtCategoryID.TabIndex = 1;
+			// 
+			// btnList
+			// 
+			this.btnList.Location = new System.Drawing.Point(164, 189);
+			this.btnList.Name = "btnList";
+			this.btnList.Size = new System.Drawing.Size(198, 54);
+			this.btnList.TabIndex = 2;
+			this.btnList.Text = "Listele";
+			this.btnList.UseVisualStyleBackColor = true;
+			// 
+			// datagridview1
+			// 
+			this.datagridview1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.datagridview1.Location = new System.Drawing.Point(380, 54);
+			this.datagridview1.Name = "datagridview1";
+			this.datagridview1.RowHeadersWidth = 62;
+			this.datagridview1.RowTemplate.Height = 28;
+			this.datagridview1.Size = new System.Drawing.Size(702, 648);
+			this.datagridview1.TabIndex = 3;
+			// 
+			// txtCategoryName
+			// 
+			this.txtCategoryName.Location = new System.Drawing.Point(164, 97);
+			this.txtCategoryName.Name = "txtCategoryName";
+			this.txtCategoryName.Size = new System.Drawing.Size(198, 26);
+			this.txtCategoryName.TabIndex = 5;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(56, 103);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(99, 20);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Kategori Adı:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(35, 148);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(120, 20);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Kategori Durum";
+			// 
+			// rdbActive
+			// 
+			this.rdbActive.AutoSize = true;
+			this.rdbActive.Location = new System.Drawing.Point(165, 144);
+			this.rdbActive.Name = "rdbActive";
+			this.rdbActive.Size = new System.Drawing.Size(66, 24);
+			this.rdbActive.TabIndex = 7;
+			this.rdbActive.TabStop = true;
+			this.rdbActive.Text = "Aktif";
+			this.rdbActive.UseVisualStyleBackColor = true;
+			// 
+			// rdbPassive
+			// 
+			this.rdbPassive.AutoSize = true;
+			this.rdbPassive.Location = new System.Drawing.Point(237, 144);
+			this.rdbPassive.Name = "rdbPassive";
+			this.rdbPassive.Size = new System.Drawing.Size(69, 24);
+			this.rdbPassive.TabIndex = 8;
+			this.rdbPassive.TabStop = true;
+			this.rdbPassive.Text = "Pasif";
+			this.rdbPassive.UseVisualStyleBackColor = true;
+			// 
+			// BtnAdd
+			// 
+			this.BtnAdd.Location = new System.Drawing.Point(165, 249);
+			this.BtnAdd.Name = "BtnAdd";
+			this.BtnAdd.Size = new System.Drawing.Size(198, 54);
+			this.BtnAdd.TabIndex = 9;
+			this.BtnAdd.Text = "Ekle";
+			this.BtnAdd.UseVisualStyleBackColor = true;
+			// 
+			// BtnDelete
+			// 
+			this.BtnDelete.Location = new System.Drawing.Point(165, 309);
+			this.BtnDelete.Name = "BtnDelete";
+			this.BtnDelete.Size = new System.Drawing.Size(198, 54);
+			this.BtnDelete.TabIndex = 10;
+			this.BtnDelete.Text = "Sil";
+			this.BtnDelete.UseVisualStyleBackColor = true;
+			// 
+			// BtnUpdate
+			// 
+			this.BtnUpdate.Location = new System.Drawing.Point(164, 369);
+			this.BtnUpdate.Name = "BtnUpdate";
+			this.BtnUpdate.Size = new System.Drawing.Size(198, 54);
+			this.BtnUpdate.TabIndex = 11;
+			this.BtnUpdate.Text = "Güncelle";
+			this.BtnUpdate.UseVisualStyleBackColor = true;
+			// 
+			// btnGetById
+			// 
+			this.btnGetById.Location = new System.Drawing.Point(164, 429);
+			this.btnGetById.Name = "btnGetById";
+			this.btnGetById.Size = new System.Drawing.Size(198, 54);
+			this.btnGetById.TabIndex = 12;
+			this.btnGetById.Text = "Id\'ye Göre Getir";
+			this.btnGetById.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1103, 732);
+			this.Controls.Add(this.btnGetById);
+			this.Controls.Add(this.BtnUpdate);
+			this.Controls.Add(this.BtnDelete);
+			this.Controls.Add(this.BtnAdd);
+			this.Controls.Add(this.rdbPassive);
+			this.Controls.Add(this.rdbActive);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.txtCategoryName);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.datagridview1);
+			this.Controls.Add(this.btnList);
+			this.Controls.Add(this.txtCategoryID);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.datagridview1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtCategoryID;
+		private System.Windows.Forms.Button btnList;
+		private System.Windows.Forms.DataGridView datagridview1;
+		private System.Windows.Forms.TextBox txtCategoryName;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.RadioButton rdbActive;
+		private System.Windows.Forms.RadioButton rdbPassive;
+		private System.Windows.Forms.Button BtnAdd;
+		private System.Windows.Forms.Button BtnDelete;
+		private System.Windows.Forms.Button BtnUpdate;
+		private System.Windows.Forms.Button btnGetById;
+	}
+}
+
